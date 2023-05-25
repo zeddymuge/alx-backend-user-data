e925f28a478b2bc28481516d0de7beb47cc38d9b
zeddy cherotich <107035298+zeddymuge@users.noreply.github.com>
2023-05-25 20:52:47 +0300

    done

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""A module for encrypting passwords.
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Returns a salted, hashed password, which is a byte string.
+    """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if a provided password matches a hashed password.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
